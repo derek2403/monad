50a1dee94bb23482150291dd84f1db84153b140a
Suet Ling Chow <csl.chowling@gmail.com>
2026-02-17 16:13:19 -0700

    redpacket

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Game from './components/Game/Game'
+import RedPacket from './components/RedPacket/RedPacket'
 
 export default function App() {
-  return <Game />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Game />} />
+        <Route path="/redpacket" element={<RedPacket />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
