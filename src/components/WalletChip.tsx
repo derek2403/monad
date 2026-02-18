38cdc87e2db662128a572b9730f2649e52fa5ddb
JingYuan <jingyuan0926@gmail.com>
2026-02-17 18:07:36 -0700

    Edit wallet chips

--- src/components/WalletChip.tsx
+++ src/components/WalletChip.tsx
@@ -26,6 +26,9 @@ export default function WalletChip({ address, onPress }: WalletChipProps) {
       }}
     >
       {short}
+      <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" style={{ opacity: 0.4 }}>
+        <polyline points="9 18 15 12 9 6" />
+      </svg>
     </button>
   )
 }
